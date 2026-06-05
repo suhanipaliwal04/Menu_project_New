4f666278becb4d9a4e24fe81f2531730fe2346d6
suhanipaliwal04 <suhanipaliwal04@users.noreply.github.com>
2026-06-05 11:27:51 +0000

    deploy: cea302175fe0f5a413993f3f73dc19a762b3db8f

--- main.dart.js
+++ main.dart.js
@@ -33376,7 +33376,7 @@ q=s[1]
 s=s[2]
 p=b[2]
 return A.b([o*m+l*k+j*n,o*r+l*q+j*s,o*p[0]+l*p[1]+j*p[2]],t.n)},
-b7X(){return"http://127.0.0.1:8000/api/v1"},
+b7X(){return"https://eatbot-lifa.onrender.com/api/v1"},
 b7Y(a){if(a==null)return B.U
 if(a>=7)return B.L8
 if(a>=4)return B.r4
@@ -107447,7 +107447,7 @@ s=o==null?s:o}catch(n){}throw A.f(new A.dx(s,m))},
 GJ(){var s=0,r=A.w(t.y),q,p=2,o=[],n=this,m,l,k,j,i
 var $async$GJ=A.r(function(a,b){if(a===1){o.push(b)
 s=p}for(;;)switch(s){case 0:p=4
-m=A.eN(A.ep("http://127.0.0.1:8000/api/v1","/api/v1","")+"/health",0,null)
+m=A.eN(A.ep("https://eatbot-lifa.onrender.com/api/v1","/api/v1","")+"/health",0,null)
 k=t.N
 s=7
 return A.m(n.b.Nr("GET",m,A.ag(["Content-Type","application/json","Accept","application/json"],k,k)).pj(0,B.dY),$async$GJ)
